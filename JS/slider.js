4ae0b5849d86e2cddaa39d7e83574ac5d1be15e2
Hyde Francois Khamsing <francoiskhamsing@gmail.com>
2023-11-27 21:28:20 +0100

    added slider zoom feature
    added small tweaks

--- JS/slider.js
+++ JS/slider.js
@@ -34,13 +34,38 @@ function goToSlide(n) {
   showSlides();
 }
 
+function showOverlay(imageSrc) {
+  const overlay = document.createElement('div');
+  overlay.className = 'overlay';
+
+  const clickedSlide = document.createElement('img');
+  clickedSlide.src = imageSrc;
+  clickedSlide.className = 'enlarged-slide';
+
+  overlay.appendChild(clickedSlide);
+  document.body.appendChild(overlay);
+
+  overlay.addEventListener('click', () => {
+    document.body.removeChild(overlay);
+  });
+}
+
 // Set interval for automatic sliding (Optional)
 setInterval(() => {
   changeSlide(1);
-}, 5000); // Change slide every 3 seconds
+}, 5000); // Change slide every 5 seconds
 
 // Resize event listener for responsiveness
 window.addEventListener('resize', showSlides);
 
 // Initial presentation
 showSlides();
+
+// Attach click event listeners to each image
+const slideImages = document.querySelectorAll('.slide img');
+slideImages.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    console.log('Image clicked:', index);
+    showOverlay(image.src);
+  });
+});
